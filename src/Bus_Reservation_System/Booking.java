babd8d02aaf2d65a2e68b70993a6cf4e18e00204
SRINITHYA04 <sn4812648@gmail.com>
2025-08-05 23:35:35 +0530

    Completed Simple Bus Reservation system project

--- src/Bus_Reservation_System/Booking.java
+++ src/Bus_Reservation_System/Booking.java
@@ -1,5 +1,7 @@
 package Bus_Reservation_System;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Booking {
@@ -7,4 +9,43 @@ public class Booking {
     int busNo;
     Date date;
 
+    Booking() throws ParseException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the passenger name: ");
+        passengerName = sc.nextLine();
+
+        System.out.println("Enter the Bus Number: ");
+        busNo = sc.nextInt();
+
+        System.out.println("Enter the Date as (dd-mm-yyy): ");
+        String dateInput = sc.next();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        try{
+            date = dateFormat.parse(dateInput);
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public  boolean isAvailable(ArrayList<Booking> bookings, ArrayList<Bus> buses) {
+        int capacity=0;
+        int booked =0;
+
+        for(Bus b: buses){
+            if(b.getBusNo() == busNo){
+                capacity = b.getCapacity();
+            }
+        }
+
+        for(Booking b : bookings){
+            if(b.busNo == busNo && b.date.equals(date) ){
+                booked++;
+            }
+        }
+        return booked<capacity;
+    }
 }
+
--- src/Bus_Reservation_System/BusDemo.java
+++ src/Bus_Reservation_System/BusDemo.java
@@ -1,14 +1,17 @@
 package Bus_Reservation_System;
 
+import java.awt.print.Book;
+import java.text.ParseException;
 import java.util.*;
 
 public class BusDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         ArrayList<Bus> buses = new ArrayList<>();
-        buses.add(new Bus(1,true,45));
-        buses.add(new Bus(2,false,50));
-        buses.add(new Bus(3,true,48));
+        ArrayList<Booking> bookings = new ArrayList<>();
+        buses.add(new Bus(1,true,2));
+        buses.add(new Bus(2,false,2));
+        buses.add(new Bus(3,true,3));
         for(Bus b : buses){
             b.displayBusInfo();
         }
@@ -20,7 +23,14 @@ public class BusDemo {
             System.out.println("Enter 1 for Booking and Enter 2 for Exit.");
             userOpt = sc.nextInt();
             if(userOpt == 1){
-                System.out.println("Bookings");
+                Booking booking = new Booking();
+                if(booking.isAvailable(bookings, buses)){
+                    bookings.add(booking);
+                    System.out.println("Your Booking is successFull.");
+                }
+                else{
+                    System.out.println("Sorry bus is Full. Try another Bus or Date.");
+                }
             }
         }
     }
